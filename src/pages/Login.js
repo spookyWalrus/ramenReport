2628e608263ce623bbf58dfa8e204680ba63b72c
Zen Nakamura <zenzenzen@gmail.com>
2023-03-04 12:29:06 -0500

    Login page now changes login status

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,31 @@
-import { Form,useActionData,redirect} from "react-router-dom";
-
-
+import { Form, useActionData, redirect, Navigate} from "react-router-dom";
+import {useContext} from 'react';
+import {DataContext} from '../App';
 
 export default function Login(){
+	const {signedIn,setSignedIn} = useContext(DataContext);
+	// console.log('signedIn object: ',signedIn);
+	// set if statement in sendLoginAction so that a succesful login will return a object with key 'success' ; true.
+	// Then we use the variable data.success as means to set the value of our 'signedIn' hook to TRUE 
+	// const {status, alert} = useActionData();
+   // setSignedIn(status);
+   // alert();
+   // console.log('signed in status: ',signedIn);
+
+	const logdat = useActionData();
+	// const status = logdat.status;
+	if(logdat){
+		setSignedIn(logdat.status);
+	}
+	// console.log(logdat.alert);
+
 	return(
 		<div className="ratingBack">
 	      <div className="reportIntro">
 				<h2>This is the login</h2>
 			</div>
 			<div className="reportPage">
-				<Form method="post" action="/report">
+				<Form method="post" action="/login">
 					<label>
 						<span>Email</span>
 						<input type="email" name="email" required />
@@ -19,10 +35,10 @@ export default function Login(){
 						<input type="password" name="password" required />
 					</label>
 					<div>
-						<button className="reportSubmitButton">Submit</button>
+						<button type="submit" className="reportSubmitButton">Submit</button>
 					</div>
 
-				{data && data.error && <p>{data.error}</p>}
+				{/*{data && data.error && <p>{data.error}</p>}*/}
 				</Form>
 			</div>
 		</div>
@@ -32,26 +48,29 @@ export default function Login(){
 // export default Login;
 
 export const sendLoginAction = async({ request })=> {
-  const data = await request.formData();
+	const logindata = await request.formData();
 
-  const submission = {
-	email: data.get('email'),
-	password: data.get('password')
-  }
-  console.log(submission)
-  // send your post request
+	const login = {
+		email: logindata.get('email'),
+		password: logindata.get('password')
+	}
+	console.log(login)
 
+	// callback function on success
+	function success(){
+		 return { status: true,
+		 				alert: alert("successful login"),
+		 			}
+	}
+	// send your post request via fetch()
+	// use .then for success/unsuccesful action
+	if (login.email.length > 0){
+		redirect('/');
+		return success();
+	}
+	  // below should be catch error statement for unsuccesful login
+	if (login.password.length < 3) {
+	 return {error: 'password must be over 8 chars long.'}
+	}
 
-  // this should be async/await statement with a catch for unsuccesful login
-  if (submission.password.length < 8) {
-    return {error: 'password must be over 8 chars long.'}
-  }
-
-  // redirect the user on successful login
-   function good(){
-  	 alert("successful login");
-  	 setSignedIn(true);
-  	 return redirect('/');
-  }
-  return  good();
 }
