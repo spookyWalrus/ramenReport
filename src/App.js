182668bf5b80890c8358f40bbefd06c44a2c385a
Zen Nakamura <zenzenzen@gmail.com>
2023-03-07 11:29:00 -0500

    login functionality completed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {
       RouterProvider,
       Route } from 'react-router-dom';
 // import React, {useEffect} from 'react';
-import {useState,createContext} from 'react';
+import {useState} from 'react';
 
 //Layouts
 import RootLayout from './layout/RootLayout';
@@ -14,38 +14,43 @@ import Home from './pages/Home';
 import Report, {sendReportAction} from './pages/Report';
 import Ratings from './pages/Ratings';
 import ErrorPage from './pages/ErrorPage';
-import Login, {sendLoginAction} from './pages/Login';
+import Login from './pages/Login';
+// import Register from './pages/Register';
 
 //components
 import Know from './components/Know';
+import {DataContext,sendLoginAction} from './components/CheckNav';
+// import SendLoginAction from './components/SendLoginAction';
+
 //css
 import './App.css';
 
 
 const theRouter = createBrowserRouter(
    createRoutesFromElements(
-      // <DataContext.Provider value={{signedIn,setSignedIn}}>
          <Route exact path="/" element={<RootLayout />}
          >  
             <Route index element={<Home />}  />
             <Route path="know" element={<Know />} />
             <Route path="report" element={<Report />} action={sendReportAction}/>
             <Route path="ratings" element={<Ratings />} />
-            <Route path="login" element={<Login />} action={sendLoginAction} />
+            <Route path="login" element={<Login />}  action={sendLoginAction}/>
+            <Route path="register" element={<Login />}  action={sendLoginAction}/>
             <Route path="*" element={<ErrorPage />} />
          </Route>
-      // </DataContext.Provider>
    )
 );
 
 export default function App() {
-   const [signedIn,setSignedIn] = useState(false);
-   // const {status, alert} = useActionData();
-   // setSignedIn(status);
-   // alert();
-   if(signedIn === true){
-      console.log('signed in status: ',signedIn);
+   const [signedIn,setSignedIn] = useState();
+   // const status = {signedIn,setSignedIn};
+
+   if(signedIn === true){ 
+      console.log('signed TRUE status: ',signedIn);
+   }else{
+      console.log('signed FALSE status: ',signedIn);
    }
+
    return (
       <DataContext.Provider value={{signedIn,setSignedIn}}>
          <RouterProvider router={theRouter} />
@@ -53,5 +58,3 @@ export default function App() {
    );
 }
    
-export const DataContext = createContext({});
-
